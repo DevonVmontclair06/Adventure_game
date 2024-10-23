78b39a2aa3a853bd3188134297e1004db1fc5a68
DevonVmontclair06 <volped3@montclair.edu>
2024-10-23 22:24:35 +0000

    origin

--- true_adventure_game.py
+++ true_adventure_game.py
@@ -15,9 +15,9 @@ def scenario_1():
         print('Choose between 1a or 1b')
         user_input = input().strip()
         if user_input == '1a':
-            print(scenario_2())
+            scenario_2()
         elif user_input == '1b':
-            print('5')
+            scenario_5()
         else:
             print('Invalid answer')
 
@@ -31,9 +31,10 @@ def scenario_2():
         if user_input == '2a':
             scenario_3()
         elif user_input == '2b':
-            print('5')
+            print(end_a)
         else:
             print('Invalid answer')
+end_a = 'he'
 
 def scenario_3():
     options = ['3a','3b']
@@ -42,12 +43,13 @@ def scenario_3():
     while user_input not in options:
         print('Choose between 3a or 3b')
         user_input = input().strip()
-        if user_input == '3a':
+        if user_input == '3b':
             scenario_4()
-        elif user_input == '3b':
-            print('5')
+        elif user_input == '3a':
+            print(end_b)
         else:
             print('Invalid answer')
+end_b = 'p'
 
 def scenario_4():
     options = ['4a','4b']
@@ -57,13 +59,74 @@ def scenario_4():
         print('Choose between 4a or 4b')
         user_input = input().strip()
         if user_input == '4a':
-            print('end_c')
+            print(end_c)
         elif user_input == '4b':
             print(end_d)
         else:
             print('Invalid answer')
-end_d = ('where')
+end_c = 'why'
+end_d = 'where'
 
+def scenario_5():
+    options = ['5a','5b']
+    print('True test')
+    user_input = ''
+    while user_input not in options:
+        print('Choose between 5a or 5b')
+        user_input = input().strip()
+        if user_input == '5a':
+            scenario_7()
+        elif user_input == '5b':
+            scenario_6()
+        else:
+            print('Invalid answer')
+
+def scenario_6():
+    options = ['6a','6b']
+    print('True test')
+    user_input = ''
+    while user_input not in options:
+        print('Choose between 6a or 6b')
+        user_input = input().strip()
+        if user_input == '6a':
+            print(end_e)
+        elif user_input == '6b':
+            print(end_f)
+        else:
+            print('Invalid answer')
+end_e = 'd'
+end_f = 's'
+
+def scenario_7():
+    options = ['7a','7b']
+    print('True test')
+    user_input = ''
+    while user_input not in options:
+        print('Choose between 7a or 7b')
+        user_input = input().strip()
+        if user_input == '7a':
+            print(end_g)
+        elif user_input == '7b':
+            scenario_8()
+        else:
+            print('Invalid answer')
+end_g = 'e'
+
+def scenario_8():
+    options = ['8a','8b']
+    print('True test')
+    user_input = ''
+    while user_input not in options:
+        print('Choose between 8a or 8b')
+        user_input = input().strip()
+        if user_input == '8a':
+            print(end_h)
+        elif user_input == '8b':
+            print(end_i)
+        else:
+            print('Invalid answer')
+end_h = 'p'
+end_i = 'd'
 
 def main():
     while True:
